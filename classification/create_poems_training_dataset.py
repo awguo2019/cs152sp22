19ceffb914474a01275b34e64cb0a58242f3c0c8
Omarzintan <zintanomar@gmail.com>
2022-04-28 00:02:53 -0700

    small mofication to dataset generator

--- classification/create_poems_training_dataset.py
+++ classification/create_poems_training_dataset.py
@@ -16,9 +16,26 @@ with open("input/train.csv", 'w', encoding='UTF8', newline='') as f:
             textdata = readfile.read()
             row = ['0', textdata]
             writer.writerow(row)
-            
+
     # for ai-generated poems
     for filename in glob.glob('../ai-poems/*.txt'):
+        with open(filename, 'r') as readfile:
+            textdata = readfile.read()
+            row = ['0', textdata]
+            writer.writerow(row)
+
+with open("input/test.csv", 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    # for human generated poems
+    for filename in glob.glob('../free-verse-testing-human/*.txt'):
+        with open(filename, 'r') as readfile:
+            textdata = readfile.read()
+            row = ['0', textdata]
+            writer.writerow(row)
+
+    # for ai-generated poems
+    for filename in glob.glob('../free-verse-testing-ai/*.txt'):
         with open(filename, 'r') as readfile:
             textdata = readfile.read()
             row = ['0', textdata]
